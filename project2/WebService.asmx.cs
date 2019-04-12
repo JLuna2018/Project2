0fec0d058911f928ce0cdef60287a4363fc28dc0
Carl <ccasti17@asu.edu>
2019-04-11 19:16:02 -0700

    fixed question

--- project2/WebService.asmx.cs
+++ project2/WebService.asmx.cs
@@ -152,6 +152,140 @@ namespace project2
 
         }
 
+        //EXAMPLE OF A SELECT, AND RETURNING "COMPLEX" DATA TYPES
+        [WebMethod(EnableSession = true)]
+        public questions[] GetQuestions()
+        {
+            //check out the return type.  It's an array of Account objects.  You can look at our custom Account class in this solution to see that it's 
+            //just a container for public class-level variables.  It's a simple container that asp.net will have no trouble converting into json.  When we return
+            //sets of information, it's a good idea to create a custom container class to represent instances (or rows) of that information, and then return an array of those objects.  
+            //Keeps everything simple.
+
+            //WE ONLY SHARE ACCOUNTS WITH LOGGED IN USERS!
+            if (Session["username"] != null)
+            {
+                DataTable sqlDt = new DataTable("questions");
+
+                string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["teamnala"].ConnectionString;
+                string sqlSelect =
+                    "select questionText, questionCategory " +
+                    "from surveyquestions " +
+                    "where questionId = (select max(questionId) - 1 from surveyquestions) " + "or " +
+                                        "questionId = (select max(questionId) from surveyquestions)";
+
+                MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
+                MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
+
+                //gonna use this to fill a data table
+                MySqlDataAdapter sqlDa = new MySqlDataAdapter(sqlCommand);
+                //filling the data table
+                sqlDa.Fill(sqlDt);
+
+                //loop through each row in the dataset, creating instances
+                //of our container class mediaPost.  Fill each mediaPost with
+                //data from the rows, then dump them in a list.
+                List<questions> questionList = new List<questions>();
+                for (int i = 0; i < sqlDt.Rows.Count; i++)
+                {
+                    questionList.Add(new questions
+                    {
+                        questionText = sqlDt.Rows[i]["questionText"].ToString(),
+                        questionCategory = sqlDt.Rows[i]["questionCategory"].ToString(),
+
+                    });
+                }
+                //convert the list of posts to an array and return!
+                return questionList.ToArray();
+            }
+            else
+            {
+                //if they're not logged in, return an empty array
+                return new questions[0];
+            }
+        }
+
+        //EXAMPLE OF AN INSERT QUERY WITH PARAMS PASSED IN.  BONUS GETTING THE INSERTED ID FROM THE DB!
+        [WebMethod(EnableSession = true)]
+        public void GiveResponsesOne(string response)
+        {
+            string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["teamnala"].ConnectionString;
+            //the only thing fancy about this query is SELECT LAST_INSERT_ID() at the end.  All that
+            //does is tell mySql server to return the primary key of the last inserted row.
+            string sqlInsert1 =
+                "insert into surveyresponses (questionId, response) " +
+                "values( " +
+                "(select questionId from surveyquestions where questionId = (select max(questionId) - 1)), " +
+                "@responseValue)";
+
+            MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
+            MySqlCommand sqlCommand = new MySqlCommand(sqlInsert1, sqlConnection);
+
+            sqlCommand.Parameters.AddWithValue("@responseValue", HttpUtility.UrlDecode(response));
+
+
+            //this time, we're not using a data adapter to fill a data table.  We're just
+            //opening the connection, telling our command to "executescalar" which says basically
+            //execute the query and just hand me back the number the query returns (the ID, remember?).
+            //don't forget to close the connection!
+            sqlConnection.Open();
+            //we're using a try/catch so that if the query errors out we can handle it gracefully
+            //by closing the connection and moving on
+            try
+            {
+                int accountID = Convert.ToInt32(sqlCommand.ExecuteScalar());
+                //here, you could use this accountID for additional queries regarding
+                //the requested account.  Really this is just an example to show you
+                //a query where you get the primary key of the inserted row back from
+                //the database!
+            }
+            catch (Exception e)
+            {
+            }
+            sqlConnection.Close();
+        }
+
+        [WebMethod(EnableSession = true)]
+        public void GiveResponsesTwo(int questionId, string questionCategory, string response)
+        {
+            string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["teamnala"].ConnectionString;
+            //the only thing fancy about this query is SELECT LAST_INSERT_ID() at the end.  All that
+            //does is tell mySql server to return the primary key of the last inserted row.
+            string sqlInsert1 =
+                "insert into surveyresponses (questionId, questionCategory, response) " +
+                "values( " +
+                "(select questionId from surveyquestions where questionId = (select max(questionId) - 1)), " +
+                "(select questionCategory from surveyquestions where questionId = (select max(questionId) - 1)), " +
+                "@responseValue)";
+
+
+            MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
+            MySqlCommand sqlCommand = new MySqlCommand(sqlInsert1, sqlConnection);
+
+            sqlCommand.Parameters.AddWithValue("@questionIdValue", HttpUtility.UrlDecode(Convert.ToString(questionId)));
+            sqlCommand.Parameters.AddWithValue("@questionCategoryValue", HttpUtility.UrlDecode(questionCategory));
+            sqlCommand.Parameters.AddWithValue("@responseValue", HttpUtility.UrlDecode(response));
+
+
+            //this time, we're not using a data adapter to fill a data table.  We're just
+            //opening the connection, telling our command to "executescalar" which says basically
+            //execute the query and just hand me back the number the query returns (the ID, remember?).
+            //don't forget to close the connection!
+            sqlConnection.Open();
+            //we're using a try/catch so that if the query errors out we can handle it gracefully
+            //by closing the connection and moving on
+            try
+            {
+                int accountID = Convert.ToInt32(sqlCommand.ExecuteScalar());
+                //here, you could use this accountID for additional queries regarding
+                //the requested account.  Really this is just an example to show you
+                //a query where you get the primary key of the inserted row back from
+                //the database!
+            }
+            catch (Exception e)
+            {
+            }
+            sqlConnection.Close();
+        }
 
     }
 }
---
+++ project2/questions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace project2
+{
+    public class questions
+    {
+        public string questionText;
+        public string questionCategory;
+    }
+}
